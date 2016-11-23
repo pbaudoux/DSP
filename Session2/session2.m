6a51693144062f492bc81bc3537658af3b392486
Unknown <petitjean.ma@gmail.com>
2016-11-23 09:37:45 +0100

    Deleted useless Ced file

--- Session2/session2.m
+++ Session2/session2.m
@@ -105,5 +105,18 @@ title('Non-ideal case - 4096');
 
 % Effect of window length
 
+x = [yid, yni];
+nttf = [256 1024 4096];
+windows = [1 2 3];
+for i=1:2
+    figure
+    for j=1:3
+        for k=1:3
+            subplot(3,3,(j-1)*3+k)
+            plot(-1)
+        end
+    end
+end
+
 
 
