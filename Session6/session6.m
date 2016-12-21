0346b91958278f2a497b5ec0fbb2d9cdf452880e
Mathieu <petitjean.ma@gmail.com>
2016-12-21 10:10:29 +0100

    End ses6

--- Session6/session6.m
+++ Session6/session6.m
@@ -1,4 +1,19 @@
 clear;
 close all;
 
+[sign, Fs] = audioread('sign.wav');
+[myst, Fs2] = audioread('mystery_x.wav');
 
+
+figure;
+Ts = 1/Fs;
+N = 0.1/Ts;
+spectrogram(sign, N, round(N/2), [], Fs, 'yaxis');
+
+
+figure;
+Ts2 = 1/Fs2;
+N2 = 0.1/Ts2;
+spectrogram(sign, N2, round(N2/2), [], Fs2, 'yaxis');
+
+% The secret message is DSSP !
